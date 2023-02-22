0e90405fc5b41f131da63d188e9f0f470a1f0591
pranshi <pranshisharma999@gmail.com>
2023-02-23 03:16:43 +0530

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './components/shared/Layout';
+
+function App() {
+  return (
+      <>
+
+      <Layout><h1>brandsalelist</h1></Layout>
+      </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/shared/Layout.js
@@ -0,0 +1,16 @@
+import { Container } from 'react-bootstrap';
+import React from 'react';
+
+const Layout = (Props) => {
+  return(
+     <div><h1 style={{justifyContent: 'center', alignItems: 'center', display : 'flex'}}>BrandSaleList</h1></div>
+     <Container>{} </Container>
+  );
+}
+
+
+
+
+
+
+export default Layout;
